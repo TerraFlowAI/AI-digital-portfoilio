3786780c72e85aabab7189a5e2a3cc78f449feb2
TerraFlow05 <terraflow78@gmail.com>
2025-09-02 18:44:19 +0000

    Error: Bad header     at createConsoleError (https://6000-firebase-studi

--- src/components/portfolio/rive-orb.tsx
+++ src/components/portfolio/rive-orb.tsx
@@ -1,22 +1,16 @@
 
 'use client';
 
-import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
+import Rive from '@rive-app/react-canvas';
 
 export const RiveOrb = () => {
-  const { RiveComponent } = useRive({
-    src: 'https://public.rive.app/community/runtime-files/2156-4299-orb.riv',
-    stateMachines: 'State Machine 1',
-    layout: new Layout({
-        fit: Fit.Cover,
-        alignment: Alignment.Center,
-    }),
-    autoplay: true,
-  });
-
   return (
     <div className="w-24 h-24">
-        <RiveComponent />
+      <Rive
+        src="https://public.rive.app/community/runtime-files/2156-4299-orb.riv"
+        stateMachines="State Machine 1"
+        autoplay
+      />
     </div>
   );
 };
