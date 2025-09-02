8160c6f1a96dfdfd0913ecaa555590858fce8c0c
TerraFlow05 <terraflow78@gmail.com>
2025-09-02 18:51:29 +0000

    Fix Rive Orb Animation Not Rendering text Fix the Rive orb animation tha

--- src/components/portfolio/rive-orb.tsx
+++ src/components/portfolio/rive-orb.tsx
@@ -2,19 +2,8 @@
 'use client';
 
 import Rive from '@rive-app/react-canvas';
-import { useEffect, useState } from 'react';
 
 export const RiveOrb = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return <div className="w-24 h-24"></div>;
-  }
-
   return (
     <div className="w-24 h-24">
       <Rive
